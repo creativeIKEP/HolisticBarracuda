7333630a6974981b9937cc0729cb0212089f3a78
IKEP <34697515+creativeIKEP@users.noreply.github.com>
2021-08-31 17:55:30 +0900

    face and iris visualize test

--- Packages/HolisticBarracuda/Script/HolisticPipeline.cs
+++ Packages/HolisticBarracuda/Script/HolisticPipeline.cs
@@ -8,17 +8,26 @@ namespace MediaPipe.Holistic {
 
 public class HolisticPipeline : System.IDisposable
 {
+    public ComputeBuffer faceVertexBuffer => facePipeline.RefinedFaceVertexBuffer;
+    public ComputeBuffer leftEyeVertexBuffer => facePipeline.RawLeftEyeVertexBuffer;
+    public ComputeBuffer rightEyeVertexBuffer => facePipeline.RawRightEyeVertexBuffer;
+
     FacePipeline facePipeline;
+    public Matrix4x4 leftEyeCropMatrix;
+    public Matrix4x4 rightEyeCropMatrix;
 
-    public HolisticPipeline(){
-
+    public HolisticPipeline(MediaPipe.FaceMesh.ResourceSet faceMeshResource){
+        facePipeline = new FacePipeline(faceMeshResource);
     }
 
-    public void ProcessImage(){
-
+    public void ProcessImage(Texture inputTexture){
+        facePipeline.ProcessImage(inputTexture);
+        leftEyeCropMatrix = facePipeline.LeftEyeCropMatrix;
+        rightEyeCropMatrix = facePipeline.RightEyeCropMatrix;
     }
 
     public void Dispose(){
+        facePipeline.Dispose();
     }
 }
 
