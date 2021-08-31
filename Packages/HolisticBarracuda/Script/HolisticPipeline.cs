c93712b56ba1da709b29511d5c850e9efae0d72d
IKEP <34697515+creativeIKEP@users.noreply.github.com>
2021-08-31 16:13:23 +0900

    add keijiro faceMesh script

---
+++ Packages/HolisticBarracuda/Script/HolisticPipeline.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MediaPipe.FaceMesh;
+
+
+namespace MediaPipe.Holistic {
+
+public class HolisticPipeline : System.IDisposable
+{
+    FacePipeline facePipeline;
+
+    public HolisticPipeline(){
+
+    }
+
+    public void ProcessImage(){
+
+    }
+
+    public void Dispose(){
+    }
+}
+
+}
