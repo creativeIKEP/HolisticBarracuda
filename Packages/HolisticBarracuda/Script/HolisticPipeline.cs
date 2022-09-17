bd19330ac77eaadbc727081cda08e73d87dcf95c
IKEP <34697515+creativeIKEP@users.noreply.github.com>
2022-09-17 18:30:37 +0900

    delete hand landmark lpf

--- Packages/HolisticBarracuda/Script/HolisticPipeline.cs
+++ Packages/HolisticBarracuda/Script/HolisticPipeline.cs
@@ -109,8 +109,6 @@ public class HolisticPipeline : System.IDisposable
     ComputeBuffer leftHandRegionFromPose;
     ComputeBuffer rightHandRegionFromPose;
     ComputeBuffer handCropBuffer;
-    ComputeBuffer deltaLeftHandVertexBuffer;
-    ComputeBuffer deltaRightHandVertexBuffer;
     // Array of landmarks for accessing data with CPU (C#). 
     Vector4[] faceLandmarks, leftEyeLandmarks, rightEyeLandmarks, leftHandLandmarks, rightHandLandmarks;
     #endregion
@@ -143,8 +141,6 @@ public class HolisticPipeline : System.IDisposable
         leftHandRegionFromPose = new ComputeBuffer(1, sizeof(float) * 24);
         rightHandRegionFromPose = new ComputeBuffer(1, sizeof(float) * 24);
         handCropBuffer = new ComputeBuffer(handCropImageSize * handCropImageSize * 3, sizeof(float));
-        deltaLeftHandVertexBuffer = new ComputeBuffer(handVertexCount, sizeof(float) * 4);
-        deltaRightHandVertexBuffer = new ComputeBuffer(handVertexCount, sizeof(float) * 4);
 
         faceLandmarks = new Vector4[faceVertexCount];
         leftEyeLandmarks = new Vector4[eyeVertexCount];
@@ -170,8 +166,6 @@ public class HolisticPipeline : System.IDisposable
         leftHandRegionFromPose.Dispose();
         rightHandRegionFromPose.Dispose();
         handCropBuffer.Dispose();
-        deltaLeftHandVertexBuffer.Dispose();
-        deltaRightHandVertexBuffer.Dispose();
     }
 
     // Provide cached landmarks.
@@ -292,11 +286,9 @@ public class HolisticPipeline : System.IDisposable
         handLandmarkDetector.ProcessImage(handCropBuffer);
 
         // Key point postprocess
-        handCs.SetFloat("_handPostDt", Time.deltaTime);
         handCs.SetBuffer(2, "_handPostInput", handLandmarkDetector.OutputBuffer);
         handCs.SetBuffer(2, "_handPostRegion", isRight ? rightHandRegionFromPose : leftHandRegionFromPose);
         handCs.SetBuffer(2, "_handPostOutput", isRight ? rightHandVertexBuffer : leftHandVertexBuffer);
-        handCs.SetBuffer(2, "_handPostDeltaOutput", isRight ? deltaRightHandVertexBuffer : deltaLeftHandVertexBuffer);
         handCs.Dispatch(2, 1, 1, 1);
 
         if(isRight){
